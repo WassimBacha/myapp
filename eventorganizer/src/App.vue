b5e4841015b384932fd621ab97c55a7f9a978a99
LAPTOP-QODNUGNI\HP <wassimbacha99@gmail.com>
2022-04-04 14:37:06 +0100

    second commit

---
+++ eventorganizer/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+    <login msg="login page"/>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+
+</style>
